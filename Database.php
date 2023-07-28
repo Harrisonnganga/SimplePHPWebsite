7f0599277b135dcea01b01af9842e07f3a1a6de7
Harrisonnganga <ngangaharrison490@gmail.com>
2023-07-29 02:45:19 +0300

    testing php site

--- Database.php
+++ Database.php
@@ -3,4 +3,4 @@ $user = 'root';
 $password = '*manasseh*';
 $db = 'myapp';
 
-$db = new mysqli ('localhost', $user, $password, $db);
+$db = new mysqli ('localhost', $user, $password, $db) or die('could not  connect at the moment');
--- views/contact_form.php
+++ views/contact_form.php
@@ -8,4 +8,33 @@
   <label for="message">Message:</label>
   <textarea id="message" name="message" required></textarea><br>
   <button type="submit">Submit</button><br>
+  
 </form>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Validate form data
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  if (empty($name) || empty($email) || empty($message)) {
+    // Redirect back to the form page with an error message
+    header("Location: contact.php?error=required_fields");
+    exit;
+  }
+
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    // Redirect back to the form page with an error message
+    header("Location: contact.php?error=invalid_email");
+    exit;
+  }
+
+  // Simulate processing the form data (e.g., saving to a database)
+  // ...
+
+  // Redirect to the thank you page
+  header("Location: thank_you.php");
+  exit;
+}
+?>
