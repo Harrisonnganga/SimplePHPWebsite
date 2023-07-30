9369e2b7dd081eadeb8c52666bbe34d3cedaf8e3
Harrisonnganga <ngangaharrison490@gmail.com>
2023-07-30 10:39:24 +0300

    Update head.php

--- partials/head.php
+++ partials/head.php
@@ -6,5 +6,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://fonts.googleapis.com/css2?family=Merriweather&family=Open+Sans&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
+    
 </head>
