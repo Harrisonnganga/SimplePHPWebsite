d59846c87b122a0797188b7745bb71c65c391c7b
Harrisonnganga <ngangaharrison490@gmail.com>
2023-07-30 10:38:32 +0300

    commit

--- partials/header.php
+++ partials/header.php
@@ -1,10 +1,11 @@
-<header>
-    <a href="/" style="text-decoration: none; color: #000; margin-right: 25px;">Home</a>
-    <a href="about" style="text-decoration: none; color: #000; margin-right: 25px;">About</a>
-    <a href="contact" style="text-decoration: none; color: #000; margin-right: 25px;">Contact</a>
-    <a href="login" style="text-decoration: none; color: #000; margin-right: 25px;">Log In</a>
-    <a href="register" style="text-decoration: none; color: #000; margin-right: 25px;">Register</a>
+<header class="bg-white shadow">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <a href="/" style="text-decoration: none; color: #000; margin-right: 25px;">Home</a>
+        <a href="about" style="text-decoration: none; color: #000; margin-right: 25px;">About</a>
+        <a href="contact" style="text-decoration: none; color: #000; margin-right: 25px;">Contact</a>
+        <a href="login" style="text-decoration: none; color: #000; margin-right: 25px;">Log In</a>
+        <a href="register" style="text-decoration: none; color: #000; margin-right: 25px;">Register</a>
+    </div>
 
 
 </header>
-
